aeac3a05feb119d74a02474e665c980de1bc1225
Edouard <erucquoy@gmail.com>
2017-06-17 08:29:11 +0200

    fix: double \\n

--- server.js
+++ server.js
@@ -1,22 +1,4 @@
 var twilio_config = {
-	"name": "Click To Call: Twilio & Node",
-	"description": "An example of implementing click to call functionality in a Node.js application",
-	"keywords": [
-		"node.js",
-		"API",
-		"twilio",
-		"expressjs",
-		"Express",
-		"Tutorial",
-		"Telephone API",
-		"Voice API",
-		"REST API",
-		"Demo"
-	],
-	"website": "https://twilio.com/docs/howto/click-to-call-walkthrough",
-	"repository": "https://github.com/TwilioDevEd/clicktocall-node",
-	"logo": "https://s3-us-west-2.amazonaws.com/deved/twilio-logo.png",
-	"success_url": "/landing.html",
 	accountSid: 'ACd1550dce7f28d8a378c13b517b9bf0e8',
 	authToken: 'cba9bde18db21d91090c5c38ca13fd7d',
 	from:  '+33756797227'
@@ -24,12 +6,38 @@ var twilio_config = {
 
 var client = require('twilio')(twilio_config.accountSid, twilio_config.authToken); 
 
+function sendSMS(to, body) {
+	client.messages.create({ 
+		to: to, 
+		from: twilio_config.from, 
+		body: body, 
+	}, function(err, message) {
+		if (err !== null) {
+			console.log(err);
+		}
+		if (typeof(message) !== 'undefined') {
+			console.log(message.sid);
+		}
+	});
+}
+
+function callPhone(to) {
+	client.calls.create(options)
+		.then((message) => {
+			console.log(message.responseText);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+}
+
 var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
    res.sendFile(path.join(__dirname + '/index.html'));
@@ -43,6 +51,10 @@ app.get('/alone', function (req, res) {
    res.sendFile(path.join(__dirname + '/alone.html'));
 })
 
+app.get('/medication', function (req, res) {
+   res.sendFile(path.join(__dirname + '/medication.html'));
+})
+
 app.get('/falling', function (req, res) {
    res.sendFile(path.join(__dirname + '/falling.html'));
 })
@@ -53,27 +65,11 @@ app.get('/howareyou', function (req, res) {
 
 app.post('/sendsms', function (req, res) {
 	var data = req.body;
-	var result = {};
+	var result = {'result': true};
 	if(data.to !== undefined && data.body !== undefined) {
-		var res_sms;
-		client.messages.create({ 
-			to: data.to, 
-			from: twilio_config.from, 
-			body: data.body, 
-		}, function(err, message) {
-			if (err !== null) {
-				console.log(err);
-				result.result = false;
-				result.message = 'Can\'t send the sms (twilio error)';
-			}
-			if (typeof(message) !== 'undefined') {
-				console.log(message.sid);
-				result.result = true;
-				result.message = 'Message sent to ' + data.to;
-			}
-			res.setHeader('Content-Type', 'application/json');
-			res.json(result);
-		});
+		data.to.forEach(sendSMS(element, data.body));
+		res.setHeader('Content-Type', 'application/json');
+		res.json(result);
 	}
 	else {
 		result.result = false;
@@ -83,31 +79,18 @@ app.post('/sendsms', function (req, res) {
 	}
 })
 
-app.post('/call', function(request, response) {
-	// This should be the publicly accessible URL for your application
-	// Here, we just use the host for the application making the request,
-	// but you can hard code it or use something different if need be
-	var salesNumber = "0033614322741";
-	var url = 'http://guiedo.com:8087/outbound/' + encodeURIComponent(salesNumber)
+app.post('/call', function(req, res) {
+	var data = req.body;
+	var url = 'http://google.com';
 
 	var options = {
-		to: request.body.to,
+		to: req.body.to,
 		from: twilio_config.from,
 		url: url,
 	};
 
-	// Place an outbound call to the user, using the TwiML instructions
-	// from the /outbound route
-	client.calls.create(options)
-		.then((message) => {
-			console.log(message.responseText);
-			// Reponse
-			response.send('Thank you! We will be calling you shortly.');
-		})
-		.catch((error) => {
-			console.log(error);
-			response.status(500).send(error);
-		});
+	data.to.forEach(callPhone(element));
+	res.json({'result': true});
 });
 
 app.post('/call', function(req, res) {
